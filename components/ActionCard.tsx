ba63a82224dd7736e68ddf0062bbe60eee6ab33a
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-10-15 22:48:39 +0500

    touchable opacity

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Linking, Image} from 'react-native'
+import { StyleSheet, Text, View, Linking, Image, TouchableOpacity} from 'react-native'
 import React from 'react'
 
 const ActionCard = () => {
@@ -6,7 +6,7 @@ const ActionCard = () => {
         Linking.openURL(websiteLink)
     }
   return (
-    <View>
+    <View style={{backgroundColor: '#A4B0BD'}}>
       <Text style = {styles.headingText}>ActionCard</Text>
       <View style = {[styles.card, styles.elevatedCard]}>
     <View style = {styles.headingContainer}>
@@ -20,7 +20,23 @@ const ActionCard = () => {
     }}
     style= {styles.imageStyle}
     />
-    <View  style= {styles.bodyContainer}></View>
+    <View  style= {styles.bodyContainer}>
+      <Text numberOfLines={2}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ut harum perspiciatis voluptate aperiam, blanditiis ab nam molestias corrupti dolor, placeat sint magnam?
+      </Text>
+      </View>
+    <View  style= {styles.footerContainer}>
+      <TouchableOpacity onPress={()=> openWebsite('https://www.uicolorpicker.learncodeonline.in/')}>
+        <Text style = {styles.socialLinks}>
+          Press me
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={()=> openWebsite('https://www.instagram.com/ranaumerzulfiqar/?hl=en')}>
+        <Text style = {styles.socialLinks}>
+          Press me
+        </Text>
+      </TouchableOpacity>
+    </View>
       </View>
     </View>
   )
@@ -29,14 +45,56 @@ const ActionCard = () => {
 export default ActionCard
 
 const styles = StyleSheet.create({
+ 
   headingText: {
-    fontSize:22,
+
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8
   },
   imageStyle:{
-      height:200,
+
+      height:190,
   },
-  card:{},
-  elevatedCard: {},
-  headingContainer: {},
-  headerText:{},
+  card:{
+    width: 350,
+    height: 360,
+    borderRadius:6,
+    marginVertical:12,
+    marginHorizontal: 5,
+  },
+  elevatedCard: {
+    backgroundColor: '#00CCCD',
+    elevation:6,
+  },
+  headingContainer: {
+    height:40,
+    flexDirection:'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  headerText:{
+    color: '#000',
+    fontSize: 16,
+    fontWeight: '800',
+
+  },
+  bodyContainer: {
+    padding:10,
+  },
+  footerContainer: {
+    padding:8,
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent: 'space-evenly'
+  },
+  socialLinks:{
+    fontSize:16,
+    color: '#000',
+    backgroundColor: '#fff',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 6,
+  }
 })

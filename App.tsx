e24a2e066042e88ab5b0a5496a1e5daccb6ab08d
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-10-15 14:21:06 +0500

    removed extra element

--- App.tsx
+++ App.tsx
@@ -12,35 +12,11 @@ import {
 
 
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+
 const App = () => {
   return (
     <View>
-    <Text>Hello World</Text>
+    <Text>Hello </Text>
     </View>
   )
 }

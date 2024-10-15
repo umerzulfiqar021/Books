06ebe4b6cdcb50546c488bb9fc0611496926b91a
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-10-15 20:03:45 +0500

    before image

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const FancyCard = () => {
   return (
     <View>
-      <Text>FancyCard</Text>
+      <Text style = {styles.headingText}>FancyCard</Text>
+      <View style = {[styles.card, styles.cardElevated]} >
+        <Image
+        
+        />
+        </View>
     </View>
   )
 }
 
 export default FancyCard
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+headingText: {
+    fontSize: 20,
+},
+card: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent:'center',
+    width:100,
+    height:100,
+    borderRadius: 4,
+    margin: 8,
+
+},
+cardElevated: {
+    backgroundColor: '#CAD5E2',
+    elevation: 8,
+},
+})
